a3425ed2b6426f62673cdebbd17c75948da7eed9
Pierre-Louis Jurdana <pjurdana@1A7.42angouleme.fr>
2026-08-13 15:59:17 +0200

    ADD: documentation

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,6 +1,11 @@
 #include "vga.h"
 
+/* kernel entry point, called by _start in boot.asm
+	void return type: the kernel never returns
+	if it does, boot.asm catches it with the infinite hang loop */
 void	kmain(void)
 {
+	/* writes "42" on screen at column 0, row 0
+		15 = white foreground on black background (VGA color code) */
 	vga_putstr("42", 15, 0, 0);
 }
