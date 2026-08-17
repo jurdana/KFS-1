998a043f894f9c3bfccc64a146faa1ef2c5e7768
Pierre-Louis Jurdana <mambanoire7@gmail.com>
2026-08-17 12:14:55 +0200

    ADD: enum for the VGA Color Palettes

--- include/vga.h
+++ include/vga.h
@@ -1,7 +1,28 @@
 #ifndef VGA_H
 #define VGA_H
 
-void vga_putchar(char c, unsigned char color, int x, int y);
-void vga_putstr(char *str, unsigned char color, int x, int y);
+typedef enum {
+	VGA_BLACK = 0,
+	VGA_BLUE = 1,
+	VGA_GREEN = 2,
+	VGA_CYAN = 3,
+	VGA_RED = 4,
+	VGA_MAGENTA = 5,
+	VGA_BROWN = 6,
+	VGA_WHITE = 7,
+	VGA_GRAY = 8,
+	VGA_LIGHT_BLUE = 9,
+	VGA_LIGHT_GREEN = 10,
+	VGA_LIGHT_CYAN = 11,
+	VGA_LIGHT_RED = 12,
+	VGA_LIGHT_MAGENTA = 13,
+	VGA_YELLOW = 14,
+	VGA_BRIGHT_WHITE = 15,
+} vga_color_t;
+
+void			vga_putchar(char c, unsigned char color, int x, int y);
+void			vga_putstr(char *str, unsigned char color, int x, int y);
+
+unsigned char	vga_color(vga_color_t foregroung, vga_color_t background);
 
 #endif
