51dbdbd213ac710efe85c03152fc1ffcaf2c7d6d
yeten <mambanoire7@gmail.com>
2026-08-21 11:44:01 +0200

    ADD: Update of the cursor and modification on other functions for it

--- drivers/vga.c
+++ drivers/vga.c
@@ -1,50 +1,47 @@
 #include "vga.h"
+#include "io.h"
 
-/* pointer to the VGA text mode buffer
-	0xB8000 is the fixed memory address of the VGA buffer
-	unsigned short = 2 bytes per entry (1 byte character + 1 byte color) */
-unsigned short *vga = (unsigned short *)0xB8000;
+unsigned short	*vga = (unsigned short *)0xB8000;
+int				cursor_x = 0;
+int				cursor_y = 0;
 
-/* writes a single character at position (x, y) on screen with a given color
-	c		: ASCII character to display
-	color	: color byte (4 bits background | 4 bits foreground)
-	x		: column (0 to 79)
-	y		: row (0 to 24) */
-void vga_putchar(char c, unsigned char color, int x, int y)
+void vga_putchar(char c, unsigned char color)
 {
-	/* encodes character and color into a single 16-bit value
-		color is shifted 8 bits left to occupy the high byte
-		c occupies the low byte
-		result: [color (8 bits)][character (8bits)] */
 	unsigned short entry = (color << 8) | c;
-
-	/* writes the entry at the correct position in the buffer
-		y * 80 : moves to the correct row (80 characters per row)
-		+ x    : moves to the correct column */
-	vga[y * 80 + x] = entry;
+	vga[cursor_y * 80 + cursor_x] = entry;
+	
+	cursor_x++;
+	if(cursor_x == 80)
+	{
+		cursor_y++;
+		cursor_x = 0;
+	}
+	vga_update_cursor(cursor_x, cursor_y);
 }
 
-/* writes a null-terminated string starting at the position (x, y) with a given color
-	str		: string to display
-	color	: color byte (4 bits background | 4 bits foreground)
-	x		: column (0 to 79)
-	y		: row (0 to 24) */
-void vga_putstr(char *str, unsigned char color, int x, int y)
+void vga_putstr(char *str, unsigned char color)
 {
 	int i = 0;
 
 	while (str[i])
 	{
-		/* writes the current character at the current position */
-		vga_putchar(str[i], color, x, y);
-		i++; /* next chracter*/
-		x++; /* moves to the next column on screen */
-
-		/* handles line wrapping when the end of a raw is reached */
-		if(x == 80)
-		{
-			y++;	/* moves to the next row */
-			x = 0;	/* resets column to the beginning of the row */
-		}
+		vga_putchar(str[i], color);
+		i++;
 	}
+}
+
+unsigned char	vga_color(vga_color_t foreground, vga_color_t background)
+{
+	return ((background << 4) | foreground);
+}
+
+void	vga_update_cursor(int x, int y)
+{
+	int	position = y * 80 + x;
+
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, position >> 8);
+
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, position & 0xFF);
 }
