ad7fcd27aaa16487c08ab10547b52240aaa52e73
trans <trans@trans.myguest.virtualbox.org>
2026-08-25 12:19:46 +0200

    ADD: modification of the main to test printk

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,13 +1,19 @@
 #include "vga.h"
+#include "printk.h"
 
 void	kmain(void)
 {
 	int i = 0;
 
-	while (i < 46)
-	{
-		vga_putstr("4222222222222 7777\n", vga_color(VGA_BLACK, VGA_BRIGHT_WHITE));
-		i++;
-	}
+	vga_init(VGA_BRIGHT_WHITE, VGA_BLACK);
+
+
+	printk("Hello %s ! %d + %d = %d\n", "kernel", 21, 21, 42);
+
+	// while (i < 46)
+	// {
+	// 	vga_putstr("4222222222222 7777\n", vga_color(VGA_BLACK, VGA_BRIGHT_WHITE));
+	// 	i++;
+	// }
 
 }
