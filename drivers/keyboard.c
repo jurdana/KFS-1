51dad7e55a14df32504294524fd42ddd3c7b16fe
Pierre-Louis Jurdana <mambanoire7@gmail.com>
2026-08-27 13:22:21 +0200

    FIX: missing ;

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -1,5 +1,6 @@
 #include "keyboard.h"
 #include "vga.h"
+#include "io.h"
 
 static char keymap[] = {
 	0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,
@@ -7,7 +8,7 @@ static char keymap[] = {
 	0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
 	0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0,
 	'*', 0, ' '
-}
+};
 
 void	keyboard_handler(void)
 {
